5f9f99eeb981b35657da1d471013f5b4f0181f94
JonathanGhaly <jghaly00@gmail.com>
2026-02-16 15:00:18 +0200

    Initial service setup with Docker and CI

---
+++ src/main/java/com/JonathjanGhaly/travel/poi/TravelPoiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.JonathjanGhaly.travel.poi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPoiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPoiServiceApplication.class, args);
+	}
+
+}
